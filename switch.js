7f998403c3d760d5f00672c03107699688ac79ce
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-03-04 20:06:13 +0100

    Full

--- switch.js
+++ switch.js
@@ -1,7 +1,7 @@
 // Створіть змінну, що симулює вибір варіанта зі списку. Виводьте повідомлення залежно від обраного варіанта: "Кава", "Чай" або "Сік". Використайте switch
 const whatWant = prompt("Що ви хочете (на вибрів: Кава, Чай або Сік)");
 switch (whatWant.toLowerCase().trim()) {
-    case "кава" :
+    case "кава":
         alert("За декілька хвилин вам принесуть Каву!");
         break;
     case "чай":
@@ -47,20 +47,25 @@ switch (whatDay.toLowerCase().trim()) {
 const whatMonth = Number(prompt("Напишіть, який місяць (1-12)"));
 
 switch (whatMonth) {
-    case 1 || 2:
+    case 12:
+    case 1:
+    case 2:
         alert("Зима")
         break;
-    case 3 || 4 || 5:
+    case 3:
+    case 4:
+    case 5:
         alert("Весна")
         break;
-    case 6 || 7 || 8:
+    case 6:
+    case 7:
+    case 8:
         alert("Літо")
         break;
-    case 9 || 10 || 11:
+    case 9:
+    case 10:
+    case 11:
         alert("Осінь")
-        break;
-    case 12:
-        alert("Зима")
         break;
     default:
         alert("Немає такого місяця!");
